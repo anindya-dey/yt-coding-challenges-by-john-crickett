9f27d64f80ebfcbfdab10000888147b30f2aacc9
Anindya Dey <andy.ani1992@gmail.com>
2024-06-02 09:52:19 +0530

    add ccwc in rust

---
+++ 001.build-your-own-wc-tool/rust/ccwc/src/main.rs
@@ -0,0 +1,28 @@
+use word_count;
+
+fn main() {
+    let option = std::env::args().nth(1).expect("Option not found");
+    let file_path = std::env::args().nth(2).expect("");
+
+    let content = std::fs::read_to_string(&file_path).expect("could not read file");
+
+    if option == "-c" {
+        println!("{} {}", byte_counts(content), file_path)
+    } else if option == "-l" {
+        println!("{} {}", line_counts(content), file_path)
+    } else if option == "-w" {
+        println!("{} {}", line_counts(content), file_path)
+    }
+
+}
+
+fn byte_counts(input_text: String) -> usize {
+    return input_text.as_bytes().len();
+}
+
+fn line_counts(input_text: String) -> usize {
+    return input_text.lines().count();
+}
+
+fn word_counts(input_text: String) -> usize {
+}
